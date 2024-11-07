5185b4c35aedd25dd514713a651b04e3c2c709a3
Xyr0za <102750293+Xyr0za@users.noreply.github.com>
2024-11-07 08:46:38 +0000

    Add files via upload

---
+++ scripts/navbar.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", highlight)
+
+const col = "#7494EA"
+
+function highlight() {
+
+    let loc = window.location.pathname;
+    console.log("Page ID is: " + loc);
+    switch (loc) {
+        case "/index.html":
+            console.log("LOCATED")
+            var currentElement = document.getElementById("home");
+            break;
+
+        case "/quiz.html":
+            console.log("LOCATED")
+            var currentElement = document.getElementById("quiz");
+            break;
+        
+        case "/info.html":
+            console.log("LOCATED")
+            var currentElement = document.getElementById("info");
+            break;
+    }
+    currentElement.style.backgroundColor = col;
+    currentElement.style.borderRadius = "20px";
+}
